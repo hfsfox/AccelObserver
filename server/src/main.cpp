193bf297d4f710930996fbe992737625c3e371ca
hfsfox <darkkitsunezx128k2309@gmail.com>
2026-03-13 20:59:27 +0000

    server: new implementation

---
+++ server/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <atomic>
+#include <csignal>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+
+#include <misc/help.hpp>
+
+#ifdef ENABLE_CONFPARSER
+extern "C"
+{
+    #include <confparser.h>
+}
+#endif
+
+int main(int argc, char* argv[])
+{
+}
